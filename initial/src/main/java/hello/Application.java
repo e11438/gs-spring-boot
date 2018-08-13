a1bc8d8802ed16b59b803c6dc6d9ae7989e4e33f
e11438 <isankalpa7@gmail.com>
2018-08-13 23:13:36 +0530

    changed text

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,10 +1,10 @@
 package hello;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +12,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        System.out.println("Let's inspect the beans provided by Spring Boot my version:");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
